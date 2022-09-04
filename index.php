d4887faf6162634957b529e322b3aad6c0580812
Hasti Maleki <hasti.ml19@gmail.com>
2022-09-04 14:51:25 +0430

    add edit and delete

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
 include 'DbConnect.php';
 $objDb = new DbConnect;
 $conn = $objDb->connect();
@@ -29,7 +31,7 @@ switch ($method) {
 
     case "POST":
         $task = json_decode(file_get_contents('php://input'));
-        $sql = "INSERT INTO tasks(id,title,description,status) VALUES (null,:title,:description,'Todo')";
+        $sql = "INSERT INTO tasks(id,title,description,status) VALUES (null,:title,:description,'ToDo')";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':title', $task->title);
         $stmt->bindParam(':description', $task->description);
@@ -40,9 +42,25 @@ switch ($method) {
             $reponse = ['status' => 0, 'message' => 'Failed to create record.'];
         }
         break;
+    case "PUT":
+        $task = json_decode(file_get_contents('php://input'));
+        //UPDATE `tasks` SET `title` = 'onegdfgd' WHERE `tasks`.`id` = 18;
+        $sql = "UPDATE `tasks` SET `title` =:title, `description` =:description, `status` =:status WHERE `tasks`.`id` =:id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $task->id);
+        $stmt->bindParam(':title', $task->title);
+        $stmt->bindParam(':description', $task->description);
+        $stmt->bindParam(':status', $task->status);
+        if ($stmt->execute()) {
+            $reponse = ['status' => 1, 'message' => 'Record updated successfully.'];
+        } else {
+            $reponse = ['status' => 0, 'message' => 'Failed to update record.'];
+        }
+        break;
         // $created_at = date('Y-m-d');
     case "DELETE":
-        $sql = "DELETE * FROM tasks WHERE id = :id";
+        //DELETE FROM `tasks` WHERE `tasks`.`id` = 17
+        $sql = "DELETE FROM `tasks` WHERE `tasks`.`id` = :id";
         $path = explode('/', $_SERVER['REQUEST_URI']);
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':id', $path[3]);
